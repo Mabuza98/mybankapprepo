2bf803e44ca540e1da15d35f1cb5b83dfad26078
Mabuza98 <70321095+Mabuza98@users.noreply.github.com>
2020-11-12 12:32:35 +0200

    updates

--- Assignmentbank6Ims/Assignmentbank6/Assignmentbank6/MainPage.xaml.cs
+++ Assignmentbank6Ims/Assignmentbank6/Assignmentbank6/MainPage.xaml.cs
@@ -11,43 +11,44 @@ namespace Assignmentbank6
 {
     public partial class MainPage : ContentPage
     {
-
         Bank fnb;
         Customer myNewCustomer;
         BankAccount account;
+
         public MainPage()
         {
             InitializeComponent();
-            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
-            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
             fnb.AddCustomer(myNewCustomer);
-
-            var account = myNewCustomer.ApplyForBankAccount();
-            account.DepositMoney(1500, DateTime.Now, "Stipend");
-
-            // Example of a code change
+            account = myNewCustomer.ApplyForBankAccount();
 
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            decimal amount = Decimal.Parse(DepositAmount.Text.ToString());
-            String reason = DepositReason.Text.ToString();
-            account.DepositMoney(amount, DateTime.Now, reason);
-
+            if (DepositAmount.Text != null)
+            {
+                decimal amount = Decimal.Parse(DepositAmount.Text);
+                string reason = DepositReason.Text.ToString();
+                account.DepositMoney(amount, DateTime.Now, reason);
+            }
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
             decimal amount = Decimal.Parse(WithdrawAmount.Text.ToString());
             string reason = WithdrawReason.Text.ToString();
+
             account.WithdrawMoney(amount, DateTime.Now, reason);
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
         {
             string history = account.GetTransactionHistory();
-            DisplayAlert("Transaction History", history, "Complete");
+            DisplayAlert("Transaction History", history, "OK");
         }
+
+
     }
 }
